ab6840e9017dcbcf3b4b125df584f9bc7f2b874b
Aahmed-Hossain <aahmdctec@gmail.com>
2023-11-30 06:53:29 +0600

    jwt token applied

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middle ware
-app.use(cors());
+app.use(cors({origin: ['http://localhost:5173'],credentials: true,
+}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ukrdjza.mongodb.net/?retryWrites=true&w=majority`;
@@ -28,9 +29,36 @@ async function run() {
       .db("verderaRealEstateDB")
       .collection("properties");
     const userCollection = client.db("verderaRealEstateDB").collection("users");
-    const offerCollection = client
-      .db("verderaRealEstateDB")
-      .collection("offers");
+    const offerCollection = client.db("verderaRealEstateDB").collection("offers");
+
+      const verifyToken = (req, res, next) => {
+        const token = req.cookies.token;
+        if (!token) {
+          return res.status(401).send({ message: "Not authorized" });
+        }
+        jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, (err, decoded) => {
+          if (err) {
+            return res.status(401).send({ message: "Unauthorized" });
+          }
+          // if token valid it would be decoded
+          req.user = decoded;
+          next();
+        });
+      };
+      
+      app.post('/jwt', async(req,res)=> {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.SECRET_ACCESS_TOKEN, {expiresIn: '6h'});
+        res.cookie('token', token, {httpOnly:true, secure:true, sameSite: 'none'}).send({success:true})
+      })
+
+      app.post("/logout", async (req, res) => {
+        const user = req.body;
+        res.clearCookie("token", { maxAge: 0, 
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          }).send({ success: true });
+      });
     // properties related api
     app.get("/properties", async (req, res) => {
       const result = await propertyCollection.find().toArray();
@@ -44,7 +72,7 @@ async function run() {
     });
 
     // offer related api
-    app.post("/offers", async (req, res) => {
+    app.post("/offers",verifyToken, async (req, res) => {
       const body = req.body; // body
       const offer = {
         ...body,
@@ -52,12 +80,12 @@ async function run() {
       const result = await offerCollection.insertOne(offer);
       res.send(result);
     });
-    app.get("/offers", async (req, res) => {
+    app.get("/offers",verifyToken,  async (req, res) => {
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
       }
-      const result = await offerCollection.find().toArray();
+      const result = await offerCollection.find(query).toArray();
       res.send(result);
     });
     app.get("/offers/:id", async (req, res) => {
@@ -66,7 +94,7 @@ async function run() {
       const result = await offerCollection.findOne(filter);
       res.send(result);
     });
-    app.delete("/offers/:id", async (req, res) => {
+    app.delete("/offers/:id",verifyToken, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const result = offerCollection.deleteOne(filter);
@@ -75,7 +103,7 @@ async function run() {
 
     // user related api
 
-    app.post("/users", async(req, res) => {
+    app.post("/users",verifyToken, async (req, res) => {
       const user = req.body;
       const query = { email: user?.email };
       const existingUser = await userCollection.findOne(query);
@@ -85,19 +113,16 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
-    app.get('/users', async(req, res)=> {
+    app.get("/users",verifyToken, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
-    })
-
-
-
-
-
-
-
-
-
+    });
+    app.delete("/users/:id",verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(filter);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -107,6 +132,7 @@ async function run() {
     // await client.close();
   }
 }
+
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
